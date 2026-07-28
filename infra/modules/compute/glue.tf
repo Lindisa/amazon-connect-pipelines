24691d27be5c50ed80705b4f14c78d94fa2e159e
Lindisa Bajisi <sbajisi@gmail.com>
2026-07-28 11:08:15 +0200

    Refactor Glue resources for contact evaluations

--- infra/modules/compute/glue.tf
+++ infra/modules/compute/glue.tf
@@ -1,9 +1,10 @@
- #    ============================================================
+```hcl
+# ============================================================
 # GLUE SECURITY CONFIGURATION
 # ============================================================
 
-resource "aws_glue_security_configuration" "glue_sec_config" {
-  name = "${var.resource_prefix}-${var.environment}-afs1-contact-evaluations-sec-config"
+resource "aws_glue_security_configuration" "ce_sec_config" {
+  name = "${var.resource_prefix}-${var.environment}-afs1-ce-sec-config"
 
   encryption_configuration {
     cloudwatch_encryption {
@@ -41,18 +42,11 @@ data "aws_glue_connection" "redshift_connection" {
 
 
 # ============================================================
-# NETWORK GLUE CONNECTION REQUIRED FOR SCP/VPC CONTEXT
+# EXISTING SHARED NETWORK GLUE CONNECTION
 # ============================================================
 
-resource "aws_glue_connection" "network_connection" {
-  name            = "${var.resource_prefix}-${var.environment}-afs1-contact-evaluations-network-connection"
-  connection_type = "NETWORK"
-
-  physical_connection_requirements {
-    availability_zone      = var.glue_connection_availability_zone
-    subnet_id              = var.glue_connection_subnet_id
-    security_group_id_list = var.glue_connection_name_security_group_id_list
-  }
+data "aws_glue_connection" "network_connection" {
+  id = "${data.aws_caller_identity.current.account_id}:${var.network_glue_connection_name}"
 }
 
 
@@ -60,8 +54,8 @@ resource "aws_glue_connection" "network_connection" {
 # CONTACT EVALUATIONS PREPROCESS GLUE JOB
 # ============================================================
 
-resource "aws_glue_job" "preprocess_job" {
-  name              = "${var.resource_prefix}-${var.environment}-afs1-contact-evaluations-pre-process"
+resource "aws_glue_job" "ce_preprocess_job" {
+  name              = "${var.resource_prefix}-${var.environment}-afs1-ce-pre-process"
   role_arn          = var.glue_preprocess_role_arn
   glue_version      = "5.0"
   max_retries       = 0
@@ -81,29 +75,29 @@ resource "aws_glue_job" "preprocess_job" {
   }
 
   connections = [
-    aws_glue_connection.network_connection.name,
+    data.aws_glue_connection.network_connection.name,
     data.aws_glue_connection.redshift_connection.name
   ]
 
   default_arguments = {
-    "--enable-metrics"                  = "true"
-    "--enable-spark-ui"                 = "true"
-    "--enable-auto-scaling"             = "true"
-    "--enable-job-insights"             = "true"
-    "--enable-observability-metrics"     = "true"
-    "--enable-glue-datacatalog"          = "true"
-    "--job-bookmark-option"              = "job-bookmark-enable"
-    "--job-language"                     = "python"
-    "--initial_load"                     = "false"
+    "--enable-metrics"               = "true"
+    "--enable-spark-ui"              = "true"
+    "--enable-auto-scaling"          = "true"
+    "--enable-job-insights"          = "true"
+    "--enable-observability-metrics" = "true"
+    "--enable-glue-datacatalog"      = "true"
+    "--job-bookmark-option"          = "job-bookmark-enable"
+    "--job-language"                 = "python"
+    "--initial_load"                 = "false"
 
-    "--source_bucket_name"               = var.temp_bucket
-    "--source_prefix"                    = var.source_prefix
-    "--target_bucket_name"               = var.temp_bucket
-    "--target_prefix"                    = "connect/absa-contact-centre-sit/ContactEvaluations/pre-processed/"
-    "--TempDir"                          = "s3://${var.temp_bucket}/connect/absa-contact-centre-sit/ContactEvaluations/temp/"
+    "--source_bucket_name" = var.temp_bucket
+    "--source_prefix"      = var.source_prefix
+    "--target_bucket_name" = var.temp_bucket
+    "--target_prefix"      = "connect/absa-contact-centre-${var.environment}/ContactEvaluations/pre-processed/"
+    "--TempDir"            = "s3://${var.temp_bucket}/connect/absa-contact-centre-${var.environment}/ContactEvaluations/temp/"
   }
 
-  security_configuration = aws_glue_security_configuration.glue_sec_config.name
+  security_configuration = aws_glue_security_configuration.ce_sec_config.name
 }
 
 
@@ -111,15 +105,15 @@ resource "aws_glue_job" "preprocess_job" {
 # CONTACT EVALUATIONS GLUE CRAWLER
 # ============================================================
 
-resource "aws_glue_crawler" "contact_evaluations_crawler" {
-  name          = "${var.resource_prefix}-${var.environment}-afs1-contact-evaluations-crawler"
+resource "aws_glue_crawler" "ce_crawler" {
+  name          = "${var.resource_prefix}-${var.environment}-afs1-ce-crawler"
   role          = var.glue_crawler_role_arn
   database_name = var.glue_catalog_database
 
   table_prefix = "contactevaluations_"
 
   s3_target {
-    path = "s3://${var.temp_bucket}/onnect/absa-contact-centre-sit/ContactEvaluations/pre-processed/"
+    path       = "s3://${var.temp_bucket}/connect/absa-contact-centre-${var.environment}/ContactEvaluations/pre-processed/"
     exclusions = ["**/temporary/**"]
   }
 
@@ -148,8 +142,8 @@ resource "aws_glue_crawler" "contact_evaluations_crawler" {
 # CONTACT EVALUATIONS REDSHIFT LOAD GLUE JOB
 # ============================================================
 
-resource "aws_glue_job" "redshift_job" {
-  name              = "${var.resource_prefix}-${var.environment}-afs1-contact-evaluations-redshift-etl"
+resource "aws_glue_job" "ce_redshift_job" {
+  name              = "${var.resource_prefix}-${var.environment}-afs1-ce-redshift-etl"
   role_arn          = var.glue_redshift_role_arn
   glue_version      = "5.0"
   max_retries       = 0
@@ -169,30 +163,30 @@ resource "aws_glue_job" "redshift_job" {
   }
 
   connections = [
-    aws_glue_connection.network_connection.name,
+    data.aws_glue_connection.network_connection.name,
     data.aws_glue_connection.redshift_connection.name
   ]
 
   default_arguments = {
-    "--enable-metrics"                  = "true"
-    "--enable-spark-ui"                 = "true"
-    "--enable-auto-scaling"             = "true"
-    "--enable-job-insights"             = "true"
-    "--enable-observability-metrics"     = "true"
-    "--enable-glue-datacatalog"          = "true"
-    "--initial_load"                     = "false"
-    "--job-bookmark-option"              = "job-bookmark-enable"
-    "--job-language"                     = "python"
+    "--enable-metrics"               = "true"
+    "--enable-spark-ui"              = "true"
+    "--enable-auto-scaling"          = "true"
+    "--enable-job-insights"          = "true"
+    "--enable-observability-metrics" = "true"
+    "--enable-glue-datacatalog"      = "true"
+    "--initial_load"                 = "false"
+    "--job-bookmark-option"          = "job-bookmark-enable"
+    "--job-language"                 = "python"
 
-    "--source_database"                  = var.glue_catalog_database
-    "--source_table"                     = var.glue_catalog_table
-    "--target_table"                     = var.redshift_target_table
-    "--target_connection_name"           = data.aws_glue_connection.redshift_connection.name
-    "--job_store_bucket_name"            = "s3://${var.temp_bucket}/onnect/absa-contact-centre-sit/ContactEvaluations/redshift-temp/"
-    "--redshift_s3_role_arn"              = var.redshift_role_arn
+    "--source_database"        = var.glue_catalog_database
+    "--source_table"           = var.glue_catalog_table
+    "--target_table"           = var.redshift_target_table
+    "--target_connection_name" = data.aws_glue_connection.redshift_connection.name
+    "--job_store_bucket_name"  = "s3://${var.temp_bucket}/connect/absa-contact-centre-${var.environment}/ContactEvaluations/redshift-temp/"
+    "--redshift_s3_role_arn"    = var.redshift_role_arn
   }
 
-  security_configuration = aws_glue_security_configuration.glue_sec_config.name
+  security_configuration = aws_glue_security_configuration.ce_sec_config.name
 }
 
 
@@ -200,14 +194,14 @@ resource "aws_glue_job" "redshift_job" {
 # TRIGGER: RUN PREPROCESS EVERY 30 MINUTES
 # ============================================================
 
-resource "aws_glue_trigger" "preprocess_schedule_trigger" {
-  name              = "${var.resource_prefix}-${var.environment}-afs1-contact-evaluations-preprocess-schedule"
+resource "aws_glue_trigger" "ce_preprocess_schedule_trigger" {
+  name              = "${var.resource_prefix}-${var.environment}-afs1-ce-preprocess-schedule"
   type              = "SCHEDULED"
   schedule          = "cron(0/30 * * * ? *)"
   start_on_creation = false
 
   actions {
-    job_name = aws_glue_job.preprocess_job.name
+    job_name = aws_glue_job.ce_preprocess_job.name
   }
 }
 
@@ -216,18 +210,18 @@ resource "aws_glue_trigger" "preprocess_schedule_trigger" {
 # TRIGGER: PREPROCESS -> CRAWLER
 # ============================================================
 
-resource "aws_glue_trigger" "crawler_trigger" {
-  name              = "${var.resource_prefix}-${var.environment}-afs1-contact-evaluations-crawler-trigger"
+resource "aws_glue_trigger" "ce_crawler_trigger" {
+  name              = "${var.resource_prefix}-${var.environment}-afs1-ce-crawler-trigger"
   type              = "CONDITIONAL"
   start_on_creation = false
 
   actions {
-    crawler_name = aws_glue_crawler.contact_evaluations_crawler.name
+    crawler_name = aws_glue_crawler.ce_crawler.name
   }
 
   predicate {
     conditions {
-      job_name = aws_glue_job.preprocess_job.name
+      job_name = aws_glue_job.ce_preprocess_job.name
       state    = "SUCCEEDED"
     }
   }
@@ -238,19 +232,20 @@ resource "aws_glue_trigger" "crawler_trigger" {
 # TRIGGER: CRAWLER -> REDSHIFT LOAD
 # ============================================================
 
-resource "aws_glue_trigger" "redshift_trigger" {
-  name              = "${var.resource_prefix}-${var.environment}-afs1-contact-evaluations-redshift-trigger"
+resource "aws_glue_trigger" "ce_redshift_trigger" {
+  name              = "${var.resource_prefix}-${var.environment}-afs1-ce-redshift-trigger"
   type              = "CONDITIONAL"
   start_on_creation = false
 
   actions {
-    job_name = aws_glue_job.redshift_job.name
+    job_name = aws_glue_job.ce_redshift_job.name
   }
 
   predicate {
     conditions {
-      crawler_name = aws_glue_crawler.contact_evaluations_crawler.name
+      crawler_name = aws_glue_crawler.ce_crawler.name
       crawl_state  = "SUCCEEDED"
     }
   }
 }
+```
