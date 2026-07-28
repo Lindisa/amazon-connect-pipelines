47717eec1efd3eb77928993e6f2f6f142c188e7f
Lindisa Bajisi <sbajisi@gmail.com>
2026-07-28 11:21:06 +0200

    Refactor main.tf by removing modules and adding variable
    Removed unused storage and security module definitions. Added network glue connection name variable.

--- infra/env/sit/main.tf
+++ infra/env/sit/main.tf
@@ -1,28 +1,3 @@
-module "storage" {
-  source = "../../modules/storage"
-
-  resource_prefix = var.resource_prefix
-
-  environment       = var.environment
-  source_bucket_name = var.source_bucket_name
-  target_bucket_name = var.target_bucket_name
-  glue_crawler_role_arn = module.security.glue_preprocess_role_arn
-}
-
-module "security" {
-  source = "../../modules/security"
-
-  resource_prefix = var.resource_prefix
-
-  environment               = var.environment
-  source_bucket_name        = var.source_bucket_name
-  target_bucket_name        = var.target_bucket_name
-  glue_scripts_bucket_name  = var.glue_scripts_bucket_name
-  s3_script_store_kms_key_arn = var.s3_script_store_kms_key_arn
-
-  kms_key_arn = var.kms_key_arn
-}
-
 module "compute" {
   source = "../../modules/compute"
 
@@ -34,10 +9,7 @@ module "compute" {
   redshift_role_arn        = var.redshift_role_arn
   glue_crawler_role_arn    = var.glue_crawler_role_arn
 
-  glue_connection_availability_zone     = var.glue_connection_availability_zone
-  glue_connection_subnet_id             = var.glue_connection_subnet_id
-  glue_connection_name_security_group_id_list = var.glue_connection_name_security_group_id_list
-  s3_script_store_kms_key_arn           = var.s3_script_store_kms_key_arn
+  s3_script_store_kms_key_arn = var.s3_script_store_kms_key_arn
 
   scripts_bucket = var.glue_scripts_bucket_name
   temp_bucket    = var.target_bucket_name
@@ -45,10 +17,16 @@ module "compute" {
   source_prefix = var.source_prefix
 
   ############################################################
-  # EXISTING ENTERPRISE GLUE CONNECTION
+  # EXISTING ENTERPRISE REDSHIFT/JDBC GLUE CONNECTION
   ############################################################
 
   glue_connection_name = var.glue_connection_name
+
+  ############################################################
+  # EXISTING SHARED NETWORK GLUE CONNECTION
+  ############################################################
+
+  network_glue_connection_name = var.network_glue_connection_name
 
   ############################################################
   # EXISTING GLUE CATALOG
