99ab44b6cf150454ab00d546695d9b1018d35b63
Lindisa Bajisi <sbajisi@gmail.com>
2026-07-30 07:20:55 +0200

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,19 @@
 terraform import -var-file="contact-evaluations-pipeline.tfvars" "module.compute.aws_glue_security_configuration.ce_sec_config" "npsenvoicedev-dev-afs1-ce-sec-config"
 
 terraform import -var-file="contact-evaluations-pipeline.tfvars" "module.compute.aws_glue_crawler.ce_crawler" "npsenvoicedev-dev-afs1-ce-crawler"
+
+      - name: Import existing Glue security configuration
+        run: |
+          terraform import \
+            -input=false \
+            -var-file="${{ env.TFVARS_FILE }}" \
+            "module.compute.aws_glue_security_configuration.ce_sec_config" \
+            "npsenvoicedev-dev-afs1-ce-sec-config" || true
+
+      - name: Import existing Glue crawler
+        run: |
+          terraform import \
+            -input=false \
+            -var-file="${{ env.TFVARS_FILE }}" \
+            "module.compute.aws_glue_crawler.ce_crawler" \
+            "npsenvoicedev-dev-afs1-ce-crawler" || true
