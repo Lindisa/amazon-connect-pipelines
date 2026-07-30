fe2cce9936ba0369f8deac9e9a9c44dedcc1aa9d
Lindisa Bajisi <sbajisi@gmail.com>
2026-07-30 07:14:00 +0200

    Refactor terraform import commands in outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,13 +1,3 @@
-terraform import \
-  -var-file="contact-evaluations-pipeline.tfvars" \
-  'module.compute.aws_glue_security_configuration.ce_sec_config' \
-  'npsenvoicedev-dev-afs1-ce-sec-config'
+terraform import -var-file="contact-evaluations-pipeline.tfvars" "module.compute.aws_glue_security_configuration.ce_sec_config" "npsenvoicedev-dev-afs1-ce-sec-config"
 
-
-terraform import \
-  -var-file="contact-evaluations-pipeline.tfvars" \
-  'module.compute.aws_glue_crawler.ce_crawler' \
-  'npsenvoicedev-dev-afs1-ce-crawler'
-
-terraform plan \
-  -var-file="contact-evaluations-pipeline.tfvars"
+terraform import -var-file="contact-evaluations-pipeline.tfvars" "module.compute.aws_glue_crawler.ce_crawler" "npsenvoicedev-dev-afs1-ce-crawler"
