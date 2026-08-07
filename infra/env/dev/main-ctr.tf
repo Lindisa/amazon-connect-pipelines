633b51f14f6f957c4799cdc604415b246e1b3626
Lindisa Bajisi <sbajisi@gmail.com>
2026-08-07 13:06:54 +0200

    Add main-ctr.tf for compute module configuration

---
+++ infra/env/dev/main-ctr.tf
@@ -0,0 +1,52 @@
+module "compute" {
+  source = "../../modules/compute"
+
+  environment     = var.environment
+  resource_prefix = var.resource_prefix
+
+  ############################################################
+  # GLUE IAM ROLE
+  ############################################################
+
+  glue_redshift_role_arn = module.security.glue_redshift_role_arn
+
+  ############################################################
+  # S3
+  ############################################################
+
+  scripts_bucket = var.glue_scripts_bucket_name
+  temp_bucket    = var.target_bucket_name
+
+  ############################################################
+  # EXISTING ENTERPRISE REDSHIFT/JDBC GLUE CONNECTION
+  ############################################################
+
+  glue_connection_name = var.glue_connection_name
+
+  ############################################################
+  # EXISTING SHARED NETWORK GLUE CONNECTION
+  ############################################################
+
+  network_glue_connection_name = var.network_glue_connection_name
+
+  ############################################################
+  # EXISTING GLUE CATALOG
+  ############################################################
+
+  glue_catalog_database = var.glue_catalog_database
+  glue_catalog_table    = var.glue_catalog_table
+
+  ############################################################
+  # REDSHIFT
+  ############################################################
+
+  redshift_target_table  = var.redshift_target_table
+  redshift_staging_table = var.redshift_staging_table
+  redshift_role_arn      = var.redshift_role_arn
+
+  ############################################################
+  # KMS
+  ############################################################
+
+  kms_key_arn = var.kms_key_arn
+}
